432b15efbcf7b384105d42a4017e3ff21df250a8
natiq al hawmani <you@example.com>
2022-09-14 12:58:32 +0200

    first

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // int x;
+           //  int y;
+            // x = 3;
+            // y = x + 4;
+
+            // Console.WriteLine(y);
+            // Console.ReadLine();
+
+
+             Console.WriteLine("Whats is your name?");
+              Console.Write("Enter your first name: ");
+
+              string myFirstNme;
+
+             myFirstNme = Console.ReadLine();
+
+              string MyLastName;
+
+              Console.Write("Enter your last name:");
+              MyLastName = Console.ReadLine();
+
+              Console.WriteLine("hello " + myFirstNme+ " " + MyLastName);
+              Console.ReadLine();
+            
+
+
+
+
+           // Console.Write("Enter #1:");
+           // int myFirstNmae = int.Parse(Console.ReadLine());
+           // Console.Write("enter #2");
+           // int mySecondNumber = int.Parse(Console.ReadLine());
+
+           // int sum = myFirstNmae + mySecondNumber;
+           // Console.WriteLine(" the Summation is: " + sum);
+           // Console.ReadLine();
+
+            
+            
+
+
+
+        }
+    }
+}
+
